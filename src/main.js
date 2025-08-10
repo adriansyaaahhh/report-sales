c4538327260cb8363d0618542ec4e9d203186bf7
adriansyah <adriansyah2425@gmail.com>
2025-08-10 11:21:33 +0700

    dua

--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
 
-import FormSales from './components/SalesForm.vue';
-import Report from './components/LiveReport.vue';
-
-const routes = [
-  { path: '/', redirect: '/form' },
-  { path: '/form', component: FormSales },
-  { path: '/report', component: Report }
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
-
-createApp(App).use(router).mount('#app');
+createApp(App).mount('#app')
